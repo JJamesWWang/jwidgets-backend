31599792aac0f9321da5b4228307adbbb75d6099
James S. Wang <JJamesWWang@gmail.com>
2021-12-25 15:29:33 -0500

    Set debug to false

--- jwidgets/settings.py
+++ jwidgets/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-d%dsy+9v1cq%qy915je3#83w10231nd!#x)w5sf(r!7t)ip%c="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
