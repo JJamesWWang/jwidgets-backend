94d4603b1c3ac5f7726d9ebec28a13adbcfbf345
James S. Wang <JJamesWWang@gmail.com>
2021-12-25 17:29:33 -0500

    Format wordchecker

--- wordchecker/analyzer.py
+++ wordchecker/analyzer.py
@@ -19,9 +19,7 @@ def analyze(words: List[str], text: str) -> Tuple[Set[str], Set[str], Set[str]]:
                 j = i + 1
                 while not str.isspace(subtext[start + j]):
                     j += 1
-                almost.add(
-                    f"{subtext[start:start + j]} almost matches with {word}"
-                )
+                almost.add(f"{subtext[start:start + j]} almost matches with {word}")
                 break
             start = subtext.find(word[0], start + 1, len(subtext))
         else:
--- wordchecker/apps.py
+++ wordchecker/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WordcheckerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wordchecker'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "wordchecker"
