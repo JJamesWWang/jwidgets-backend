ced621ea71411bf77630e05a3255e92e5b42d0e6
James S. Wang <JJamesWWang@gmail.com>
2021-12-25 15:24:50 -0500

    Add index page

--- jwidgets/urls.py
+++ jwidgets/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import path, re_path
+from django.http import HttpResponse
 from wordchecker import views
 
 urlpatterns = [
     re_path("admin/?", admin.site.urls),
+    path("", lambda _: HttpResponse("<h1>Index</h1>")),
     re_path("wordchecker/?", views.get_analysis),
 ]
