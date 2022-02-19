1d51e6b765bf3a11ec04aa9940d4c799a226e48b
James S. Wang <JJamesWWang@gmail.com>
2022-02-19 02:08:01 -0500

    Analyzer: Provide korean and english

--- wordchecker/analyzer.py
+++ wordchecker/analyzer.py
@@ -1,27 +1,35 @@
-from typing import List, Tuple, Set
-
-
-def analyze(words: List[str], text: str) -> Tuple[Set[str], Set[str], Set[str]]:
+def analyze(words: list[str], text: str) -> tuple[set, set, set]:
     exact, almost, unmatched = set(), set(), set()
 
     for word in words:
-        if word in text:
-            exact.add(word)
+        korean, english = parse_word(word)
+        if korean in text:
+            exact.add(objectify(korean, english))
             continue
 
         subtext = text
-        start = subtext.find(word[0], 0, len(subtext))
+        start = subtext.find(korean[0], 0, len(subtext))
         while start != -1:
             i = 1
-            while word[i] == subtext[start + i]:
+            while korean[i] == subtext[start + i]:
                 i += 1
-            if i > len(word) / 2:
+            if i > len(korean) / 2:
                 j = i + 1
                 while not str.isspace(subtext[start + j]):
                     j += 1
-                almost.add(f"{subtext[start:start + j]} almost matches with {word}")
+                almost.add(objectify(korean, english))
                 break
-            start = subtext.find(word[0], start + 1, len(subtext))
+            start = subtext.find(korean[0], start + 1, len(subtext))
         else:
-            unmatched.add(word)
+            unmatched.add(objectify(korean, english))
     return exact, almost, unmatched
+
+
+def parse_word(word: str) -> tuple[str, str]:
+    korean, english = word.split("~")
+    korean = korean.replace("(", "").replace(")", "").replace("w", "").strip()
+    return korean, english
+
+
+def objectify(korean: str, english: str) -> dict:
+    return {"korean": korean, "english": english}
