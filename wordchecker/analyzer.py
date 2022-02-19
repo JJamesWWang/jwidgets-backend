8bdcded99773c6ca914367f7d15222db90401755
James S. Wang <JJamesWWang@gmail.com>
2022-02-19 03:30:27 -0500

    Analyzer: Convert to lists instead of sets now that we are using dicts

--- wordchecker/analyzer.py
+++ wordchecker/analyzer.py
@@ -1,35 +1,41 @@
-def analyze(words: list[str], text: str) -> tuple[set, set, set]:
-    exact, almost, unmatched = set(), set(), set()
+def analyze(words: list[str], text: str) -> tuple[list, list, list]:
+    exact, almost, unmatched = list(), list(), list()
 
     for word in words:
         korean, english = parse_word(word)
         if korean in text:
-            exact.add(objectify(korean, english))
+            exact.append(objectify(korean, english))
             continue
 
         subtext = text
         start = subtext.find(korean[0], 0, len(subtext))
         while start != -1:
-            i = 1
+            i = 0
             while korean[i] == subtext[start + i]:
                 i += 1
             if i > len(korean) / 2:
                 j = i + 1
                 while not str.isspace(subtext[start + j]):
                     j += 1
-                almost.add(objectify(korean, english))
+                almost.append(objectify(korean, english, subtext[start : start + j]))
                 break
             start = subtext.find(korean[0], start + 1, len(subtext))
         else:
-            unmatched.add(objectify(korean, english))
+            unmatched.append(objectify(korean, english))
     return exact, almost, unmatched
 
 
 def parse_word(word: str) -> tuple[str, str]:
     korean, english = word.split("~")
-    korean = korean.replace("(", "").replace(")", "").replace("w", "").strip()
-    return korean, english
+    korean = (
+        korean.replace("(", "")
+        .replace(")", "")
+        .replace("w", "")
+        .replace("-", "")
+        .strip()
+    )
+    return korean, english.strip()
 
 
-def objectify(korean: str, english: str) -> dict:
-    return {"korean": korean, "english": english}
+def objectify(korean: str, english: str, original: str = "") -> dict:
+    return {"korean": korean, "english": english, "original": original}
